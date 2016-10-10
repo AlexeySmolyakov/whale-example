fe0b1130a541b50ad93f7c1f323b23e280af8b1a
Alex Smolyakov <a.smolyakov@iconto.ru>
2016-10-10 16:55:38 +0300

    plugin fixes

--- client/plugins/alol/before-receipt-fixed.js
+++ client/plugins/alol/before-receipt-fixed.js
@@ -2,9 +2,9 @@
 var navigation = require('navigation');
 
 // View to be shown within handleMoment function.
-var view = 'discount-request.html';
+var view = 'client/views/alol/discount-request.html';
 
 // Function to be invoked on evo.receipt.items.fixed moment (declared in client.yaml file).
 function handleMoment(context, navigation) {
-	navigation.pushView(view);
+	navigation.pushView(view, {});
 }
